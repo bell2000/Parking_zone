0c86b32a7af87d8bb6ebc29099dcab34148bda3c
김종현 <sumei88@naver.com>
2023-10-21 12:19:22 +0900

    기초적인 html까지 있는버전

--- src/main/java/Industryacademic/project/backend/repository/PARKING_LOTRepository.java
+++ src/main/java/Industryacademic/project/backend/repository/PARKING_LOTRepository.java
@@ -4,6 +4,8 @@ import Industryacademic.project.backend.Entity.CAR;
 import Industryacademic.project.backend.Entity.PARKING_LOT;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
 
 public interface PARKING_LOTRepository extends JpaRepository<PARKING_LOT,String> {
     PARKING_LOT findByCno(String Cno);
